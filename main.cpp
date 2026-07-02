a05aac0151629de10a7709a80ad9b72243c46923
HassanIQ777 <hassaniq793@gmail.com>
2026-07-02 23:16:28 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "libutils/src/CLIParser.hpp"
+#include "libutils/src/File.hpp"
+#include "libutils/src/funcs.hpp"
+using funcs::print;
+
+int main(int argc, char *argv[]) {
+    CLIParser parser(argc, argv);
+    
+}
